2e6403674371fccc1a8c3a86a0813232cef10f87
shivajee98 <ultimateintimater@gmail.com>
2025-07-25 09:35:26 +0530

    font update

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,10 +88,7 @@ export default function RootLayout({
         <head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap"
-            rel="stylesheet"
-          />
+          <link rel="stylesheet" href="https://use.typekit.net/vvg0muu.css" />
         </head>
         <body className={`${inter.className} antialiased`} style={{ fontFamily: "'Source Sans 3', sans-serif" }}>
           <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
